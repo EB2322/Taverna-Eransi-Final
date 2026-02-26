655345f9517af21a9039e1eff18b2952eb043f9c
EB2322 <ersiberberi90@gmail.com>
2026-02-26 13:48:32 +0100

    Protect register route for admin users only

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { AuthService } from './core/auth.service';
+
+const adminOnlyGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isAdmin) {
+    return true;
+  }
+
+  if (!authService.currentSession) {
+    return router.createUrlTree(['/login']);
+  }
+
+  return router.createUrlTree(['/menu']);
+};
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'register', component: RegisterComponent, canActivate: [adminOnlyGuard] },
   { path: '**', redirectTo: '' }
 ];
 
